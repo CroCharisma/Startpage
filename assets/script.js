c8a5d01e236c9dd187c66735753daf5b4282f030
crocharimsa <cjnewco@icloud.com>
2020-12-17 00:47:46 -0600

    searchbar now uses javascript

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+let url = "http://duckduckgo.com/?q="; //the default search url
+
 function currentTime(){
     var date = new Date();
     var hour = date.getHours();
@@ -11,12 +13,34 @@ function currentTime(){
 }
 
 function updateTime(k) {
-    if (k < 10) {
-      return "0" + k;
-    }
-    else {
-      return k;
-    }
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
+
+function redirect( args ){
+  if( args.length > 1 ){
+    
+  }
+  else{
+    let search = "";
+    search += url + args[0];
+    window.location.href = (search);
+    console.log(url + args[0]);
   }
 
-  currentTime();
+  return false;
+}
+
+function eval(e){
+  if( e.keyCode == 13 ){
+    var args = [];
+    args = document.getElementById("search").value.split(";");
+    redirect(args);
+  }
+}
+
+currentTime();
